cd693c6ac2ffb959129e41f6ebf3878742d35543
muneki-aso <muneki-aso@united.jp>
2017-12-09 16:27:40 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/aso/muneki/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.aso.muneki.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
